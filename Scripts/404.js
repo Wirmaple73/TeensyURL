4aa52c1ad79aeb57b88d4eb4603581e9c61ad711
Hojjat-hk <minhojat@gmail.com>
2024-06-10 01:14:26 +0330

    Develop 404 Page

--- Scripts/404.js
+++ Scripts/404.js
@@ -4,14 +4,11 @@ const $ = document;
 
 // [+] Defining Variables
 const homeButton = $.querySelector(".page-not-found__button--home");
-const backButton = $.querySelector(".page-not-found__button--back");
+
 // [+] Functions
 function goHomeHandler(){
     location.replace("index.html");
 }
-function goBackHandler(){
-    history.back();
-}
+
 // [+] Events
 homeButton.addEventListener("click", goHomeHandler);
-backButton.addEventListener("click", goBackHandler);
